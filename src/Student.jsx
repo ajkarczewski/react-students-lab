a5d709ec36e2e0e099af09d4299137d115775128
AJKAR <karczewskiamanda@gmail.com>
2022-03-17 16:39:06 -0500

    add key prop in student.jsx

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,12 +1,21 @@
 import Score from "./Score";
 
+// Fixed formatting and added missing a key prop on <Score/>
+
+// The main purpose of keys is to help React differentiate and 
+// distinguish elements from each other, increasing its performance 
+// when diffing between the virtual and real DOM. 
+
 const Student = (props) => {
   return (
     <>
-      <h3>Name: {props.student.name}
-        <br></br>
-        Bio: {props.student.bio}</h3>
-      {props.student.scores.map(score => <Score name={props.student.name} score={score}/>)}
+      <h3>Name: {props.student.name}</h3>
+      <h3>Bio: {props.student.bio}</h3>
+
+      {props.student.scores.map((score, idx) =>
+        <Score key={idx} name={props.student.name} score={score} />
+      )}
+      
     </>
   );
 }
